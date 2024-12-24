b21accb57f67c68aa19c5c277757dc09b488f519
JosephEmeka <josephemekaonah@gmail.com>
2024-12-24 19:01:58 +0100

    fix: fix backend url

--- src/utils/Api.jsx
+++ src/utils/Api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://your-backend-url.com/api',
+    baseURL: 'https://cherry-backend-zbr2.onrender.com',
     headers: { 'Content-Type': 'application/json' },
 });
 
